255f9be2fe42178e22a9aae52dae8f1822abdf1a
Haejo Kim <112507232+heyjoekim@users.noreply.github.com>
2024-05-30 18:31:55 -0400

    changed ccm call to included test for masked values in SST

--- scripts/ccm_wrapper.py
+++ scripts/ccm_wrapper.py
@@ -30,66 +30,73 @@ def runCCM(clim, i, j, tau):
         swe_df = swe_xr.swe_level2.mean(dim='sites').to_dataframe()
         df = swe_df.join(clim_df)
         
-        # save variable names
-        var1 = df.columns[0]    # usually swe
-        var2 = df.columns[1]    # should be SST or SLP
-        df = df.dropna()
-        df = df.reset_index()
-
         # FOR JSON:
         lon = float(clim_xr.isel(lon=i).lon.values)
         lat = float(clim_xr.isel(lat=j).lat.values)
         eco = swe_xr.swe_level2.attrs['eco_region']
+       
+        if clim_df.is_null().any().any():
+            lons.append(lon)
+            lats.append(lat)
+            ecos.append(eco)
+            rhos.append(np.round(rho,3))
+        else:
+            # save variable names
+            var1 = df.columns[0]    # usually swe
+            var2 = df.columns[1]    # should be SST or SLP
+            df = df.dropna()
+            df = df.reset_index()
 
-        df_len = len(df)
 
-        # convert time to ISO-8601 as required by pyEDM
-        df['time'] = df['time'].map(lambda x: x.isoformat())
+            df_len = len(df)
 
-        # find embedd dimensions
-        d1 = EmbedDimension(
-                dataFrame=df,
-                lib=[1, 100],
-                pred=[201, df_len],
-                columns=var2,
-                target=var2,
-                showPlot=False)
+            # convert time to ISO-8601 as required by pyEDM
+            df['time'] = df['time'].map(lambda x: x.isoformat())
 
-        d2 = EmbedDimension(
-                dataFrame=df,
-                lib=[1, 100],
-                pred=[201, df_len],
-                columns=var1,
-                target=var1,
-                showPlot=False)
+            # find embedd dimensions
+            d1 = EmbedDimension(
+                    dataFrame=df,
+                    lib=[1, 100],
+                    pred=[201, df_len],
+                    columns=var2,
+                    target=var2,
+                    showPlot=False)
 
-        ed1 = d1[d1['rho'] == d1['rho'].max()]['E'].item()
-        ed2 = d2[d2['rho'] == d2['rho'].max()]['E'].item()
+            d2 = EmbedDimension(
+                    dataFrame=df,
+                    lib=[1, 100],
+                    pred=[201, df_len],
+                    columns=var1,
+                    target=var1,
+                    showPlot=False)
 
-        # Max libSize must be less than N - (E+1)
-        maxN = df_len - (ed1 + 1)
+            ed1 = d1[d1['rho'] == d1['rho'].max()]['E'].item()
+            ed2 = d2[d2['rho'] == d2['rho'].max()]['E'].item()
+
+            # Max libSize must be less than N - (E+1)
+            maxN = df_len - (ed1 + 1)
         
-        # print(f, ed1, df_len, maxN)
-        # run ccm
-        CCMresult = CCM(
-                dataFrame = df,
-                E=int(ed1),
-                tau=-tau,
-                columns=var2,
-                target=var1,
-                libSizes=[15, maxN-1, 25],
-                sample=100,
-                showPlot=False)
+            # print(f, ed1, df_len, maxN)
+            # run ccm
+            CCMresult = CCM(
+                    dataFrame = df,
+                    E=int(ed1),
+                    tau=-tau,
+                    columns=var2,
+                    target=var1,
+                    libSizes=[15, maxN-1, 25],
+                    sample=100,
+                    showPlot=False)
 
-        # if var is anchovy::sst, reads as sst influences anchovy
-        # get SST/SLP influence SWE
-        rho = CCMresult['{}:{}'.format(var1, var2)].iloc[-1]
+            # if var is anchovy::sst, reads as sst influences anchovy
+            # get SST/SLP influence SWE
+            rho = CCMresult['{}:{}'.format(var1, var2)].iloc[-1]
 
 
-        lons.append(lon)
-        lats.append(lat)
-        ecos.append(eco)
-        rhos.append(np.round(rho,3))
+            lons.append(lon)
+            lats.append(lat)
+            ecos.append(eco)
+            rhos.append(np.round(rho,3))
     
     # save results
     results = pd.DataFrame({'eco_region': ecos,
