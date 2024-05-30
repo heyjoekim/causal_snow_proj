7c2bd681ca00469232948291f7e38a11f276db0b
Haejo Kim <112507232+heyjoekim@users.noreply.github.com>
2024-05-30 14:31:25 -0400

    too many changes to list

---
+++ scripts/compile_ccm_results.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import xarray as xr
+import glob
+
+def read_ccm_outputs(clim, t):
+    # reads outputs from CCM runs and compiles for clim {SST, SLP}
+    # for lags (tau) {1,3,6}.
+    # returns one xarray for a clim and t
+
+    # create list of files
+    outputs = np.sort(glob.glob('./outputs/{}/*tau_{}.csv'.format(clim, t)))
+
+    # create empty dataframe 
+    df = pd.DataFrame()
+
+    # loop to read files in outputs 
+    for f in outputs:
+        temp_df = pd.read_csv(f)
+        
+        # concat each output to dataframe, df 
+        df = pd.concat([df, temp_df], ignore_index=True)
+    
+    # convert to xarray
+    df_multind = df.set_index(['eco_region', 'lon', 'lat'])
+    ccm_array = df_multind.to_xarray()
+    return(ccm_array)
