17de6ec1f9272c4b5156c59c86a123ea9ce3c85f
Haejo Kim <112507232+heyjoekim@users.noreply.github.com>
2024-06-13 16:11:51 -0400

    changes to run_sst.sh

--- run_sst.sh
+++ run_sst.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-singularity exec $HOME/container/causal_snow_proj.sif python run_ccm.py $1 
+# comment out line for snwo with eco_regions
+singularity exec -H ~/causal_snow_proj ~/causal_snow_proj/container/causal_snow_proj.sif python ~/causal_snow_proj/run_ccm.py -c $1 -i $2 -j $3 -t $4
+
+# comment out line for snow for entire western United States
+singularity exec -H ~/causal_snow_proj ~/causal_snow_proj/container/causal_snow_proj.sif python ~/causal_snow_proj/run_ccm_wus.py -c $1 -i $2 -j $3 -t $4
