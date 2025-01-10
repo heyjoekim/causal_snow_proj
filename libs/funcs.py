8b0ddaae0a71d9ea0e88b5f5d35d0d67e606209d
Haejo Kim <112507232+heyjoekim@users.noreply.github.com>
2025-01-10 17:37:33 -0500

    adding global python file

---
+++ libs/funcs.py
@@ -0,0 +1,21 @@
+eco_regions = ['Middle Rockies',
+               'Klamath Mountains',
+               'Sierra Nevada',
+               'Wasatch and Uinta Mountains',
+               'Southern Rockies',
+               'Idaho Batholith',
+               'Columbia Mountains/Northern Rockies',
+               'Canadian Rockies',
+               'North Cascades',
+               'Blue Mountains',
+               'Cascades',
+               'Eastern Cascades Slopes and Foothills',
+               'Central Basin and Range',
+               'Arizona/New Mexico Mountains',
+               'Northern Basin and Range']
+
+coast_ecos = ['North Cascades',
+              'Cascades',
+              'Eastern Cascades Slopes and Foothills',
+              'Columbia Mountains/Northern Rockies'
+              ]
